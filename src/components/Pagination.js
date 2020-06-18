321939e4fb58888bd8a34a1889c1b6de611a5b75
Parvathi Reddy <parvathigopavarapu@gmail.com>
2020-06-18 11:33:48 +0530

    pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -7,27 +7,18 @@ const Pagination = ({ postsPerPage, totalPosts,paginate }) => {
         pageNumbers.push(i);
     }
     return (
-        // <nav aria-label="Page navigation example">
-        //     <ul className="pagination">
-        //         {pageNumbers.map(number => (
-        //             <li key={number} className='page-item'>
-        //                 <a onClick={()=> paginate(number)} href='!#' className='page-link'>
-        //                     {number}
-        //                 </a>
-        //             </li>
-        //         ))}
-        //     </ul>
-
-        // </nav>
         <nav aria-label="Page navigation example">
-  <ul class="pagination">
-    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item"><a class="page-link" href="#">2</a></li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item"><a class="page-link" href="#">Next</a></li>
-  </ul>
-</nav>
+            <ul className="pagination">
+                {pageNumbers.map(number => (
+                    <li key={number} className='page-item'>
+                        <a onClick={()=> paginate(number)} href='!#' className='page-link'>
+                            {number}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+
+        </nav>
     )
 }
 export default Pagination;
